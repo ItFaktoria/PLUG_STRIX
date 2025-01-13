662410dccf13c6c93947defa84892a21b1acac7c
MikolajOgrzywalskiFaktoria <131371471+MikolajOgrzywalskiFaktoria@users.noreply.github.com>
2025-01-13 15:07:24 +0100

    Update SpingoConfirmNotifyInterface.php
    Zmiana wielkości liter w $idnotify i $items

--- src/SpingoWebApi/Api/SpingoConfirmNotifyInterface.php
+++ src/SpingoWebApi/Api/SpingoConfirmNotifyInterface.php
@@ -7,9 +7,9 @@ namespace Spingo\SpingoWebApi\Api;
 interface SpingoConfirmNotifyInterface
 {
     /**
-     * @param string $idNotify
-     * @param \Spingo\SpingoWebApi\Api\Data\SpingoConfirmNotifyItemInterface[] $items
+     * @param string $IdNotify
+     * @param \Spingo\SpingoWebApi\Api\Data\SpingoConfirmNotifyItemInterface[] $Items
      * @return string
      */
-    public function confirm(string $idNotify, array $items): string;
+    public function confirm(string $IdNotify, array $Items): string;
 }
